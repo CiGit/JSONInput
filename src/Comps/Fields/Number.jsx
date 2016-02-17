0e24e00c6da4aa088c09bb50393257ee85c879c2
Cyril Junod <cyril.junod@gmail.com>
2016-02-17 17:45:02 +0100

    Number: Statefull
    Can edit decimal values

--- src/Comps/Fields/Number.jsx
+++ src/Comps/Fields/Number.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import validate from '../Decorators/validator.jsx';
 import StringField from './String.jsx';
 
-function NumberField(props) {
-    const onChange = function onChange(val) {
+class NumberField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value
+        };
+        this.boundChange = this.onChange.bind(this);
+    }
+    componentWillReceiveProps(nextProps) {
+        if (this.state.value - nextProps.value) { // if values differ: update
+            this.setState({
+                value: nextProps.value
+            });
+        }
+    }
+    onChange(val) {
         const numVal = Number(val);
-        props.onChange(numVal.toString() === val ? numVal : val);
-    };
-    return (<StringField {...props}
-                         onChange={ onChange } />);
+        this.setState({
+            value: val
+        }, () => this.props.onChange(isNaN(numVal) ? val : numVal));
+    }
+    render() {
+        return (<StringField {...this.props}
+                             value={ this.state.value }
+                             onChange={ this.boundChange } />);
+    }
 }
+NumberField.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+};
 
 export default validate(NumberField);
