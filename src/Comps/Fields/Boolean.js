d6f2792d11b3b2fc2e9c15872b1a97b8bd6e5b7b
Cyril Junod <cyril.junod@gmail.com>
2016-03-03 17:26:10 +0100

    Validate boolean

--- src/Comps/Fields/Boolean.js
+++ src/Comps/Fields/Boolean.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Widget from '../Views/Widget';
 import fromDefaultValue from '../Decorators/fromDefaultValue';
+import validator from './../Decorators/validator.js';
 
 function BooleanField(props) {
     return (<Widget {...props} />);
 }
 
-export default fromDefaultValue(BooleanField);
+export default validator(fromDefaultValue(BooleanField));
