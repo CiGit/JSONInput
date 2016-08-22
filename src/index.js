15a38e09655ed7a939e5681e0a26bf8feda15e9e
Cyril Junod <cyril.junod@gmail.com>
2016-08-22 17:55:21 +0200

    !!! setDefaultWidget no more exported
    Access setDefaultWidget on default export. static property.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Container from './Comps/Container';
 import { setDefaultWidgets } from './Comps/Views';
 
+Container.setDefaultWidgets = setDefaultWidgets;
 export default Container;
-export { setDefaultWidgets };
