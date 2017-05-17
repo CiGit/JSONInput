1de3b40c6efb3898b9980b9da5ac44c1faa2ffe1
Cyril Junod <cyril.junod@gmail.com>
2017-05-17 16:42:56 +0200

    Share types ....

---
+++ typings/global.d.ts
@@ -0,0 +1,26 @@
+declare module 'baobab-react/higher-order' {
+    export const root: any;
+    export const branch: any;
+}
+declare class Baobab {
+    constructor(...args: {}[]);
+}
+declare module "baobab" {
+    export default Baobab;
+}
+
+declare module 'react-pure-render/function' {
+    export default function (args: {}[]): boolean
+}
+declare module 'jsonschema' {
+    export class Validator {
+        public attributes: { [key: string]: (...args: any[]) => any }
+        public validate: (value: {} | void, schema: {}, options: { [option: string]: {} }) => any
+    }
+    export class SchemaError {
+        constructor(message: string);
+    }
+}
+declare module 'prop-types'{
+    export default React.PropTypes;
+}
---
+++ typings/jsoninput.d.ts
@@ -0,0 +1,5 @@
+import Form from '../src/Comps/Container'
+import { setDefaultWidgets } from "../src/Comps/Views/index";
+
+export { setDefaultWidgets };
+export default Form;
