0646300d8aa8aa92119bbf570ea922e9326a57d2
Cyril Junod <cyril.junod@gmail.com>
2018-04-16 14:43:38 +0200

    type correction

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -5,107 +5,99 @@ type ErrorFn = (value: any, formValue: any) => string;
 type TYPESTRING = 'string' | 'number' | 'boolean' | 'object' | 'array' | 'null';
 
 interface BaseView {
-    type?: string | React.ComponentType<WidgetProps>;
+  type?: string | React.ComponentType<WidgetProps>;
 }
-/**
- * Schema's view property
- * @template ViewBag  defining view props.
- */
-type View<ViewBag = { [key: string]: any }> = BaseView & ViewBag;
+
 declare namespace Schema {
-    interface BASE<ViewBag = { [key: string]: any }> {
-        type?: TYPESTRING | TYPESTRING[];
-        value?: {};
-        visible?: (value: any, formValue: any, path: string[]) => boolean;
-        errored?: ErrorFn;
-        index?: number;
-        view?: View<ViewBag>;
-        required?: boolean;
-    }
-    interface String extends BASE {
-        type?: 'string' | ['string', 'null'];
-        maxLength?: number;
-        minLength?: number;
-    }
-    interface Number extends BASE {
-        type?: 'number' | ['number', 'null'];
-        maximum?: number;
-        minimum?: number;
-    }
-    interface Boolean extends BASE {
-        type?: 'boolean' | ['boolean', 'null'];
-    }
-    interface Object extends BASE {
-        type?: 'object' | ['object', 'null'];
-        properties?: { [property: string]: Schema };
-        patternProperties?: { [pattern: string]: Schema };
-        additionalProperties?: Schema;
-    }
-    interface Array extends BASE {
-        type?: 'array' | ['array', 'null'];
-        items?: Schema | Schema[];
-        additionalItems?: Schema;
-        maxItems?: number;
-        minItems?: number;
-    }
+  interface BASE<ViewBag extends BaseView = BaseView> {
+    type?: TYPESTRING | TYPESTRING[];
+    value?: {};
+    visible?: (value: any, formValue: any, path: string[]) => boolean;
+    errored?: ErrorFn;
+    index?: number;
+    view?: ViewBag;
+    required?: boolean;
+  }
+  interface String<ViewBag extends BaseView = BaseView> extends BASE<ViewBag> {
+    type?: 'string' | ['string', 'null'];
+    maxLength?: number;
+    minLength?: number;
+  }
+  interface Number<ViewBag extends BaseView = BaseView> extends BASE<ViewBag> {
+    type?: 'number' | ['number', 'null'];
+    maximum?: number;
+    minimum?: number;
+  }
+  interface Boolean<ViewBag extends BaseView = BaseView> extends BASE<ViewBag> {
+    type?: 'boolean' | ['boolean', 'null'];
+  }
+  interface Object<ViewBag extends BaseView = BaseView> extends BASE<ViewBag> {
+    type?: 'object' | ['object', 'null'];
+    properties?: { [property: string]: Schema };
+    patternProperties?: { [pattern: string]: Schema };
+    additionalProperties?: Schema;
+  }
+  interface Array<ViewBag extends BaseView = BaseView> extends BASE<ViewBag> {
+    type?: 'array' | ['array', 'null'];
+    items?: Schema | Schema[];
+    additionalItems?: Schema;
+    maxItems?: number;
+    minItems?: number;
+  }
 }
 /**
  * Schema for form's schema prop
  *
  * @template TView View's available types
  */
-type Schema =
-    | Schema.BASE
-    | Schema.String
-    | Schema.Number
-    | Schema.Boolean
-    | Schema.Array
-    | Schema.Object;
+type Schema<ViewBag extends BaseView = BaseView> =
+  | Schema.BASE<ViewBag>
+  | Schema.String<ViewBag>
+  | Schema.Number<ViewBag>
+  | Schema.Boolean<ViewBag>
+  | Schema.Array<ViewBag>
+  | Schema.Object<ViewBag>;
 declare namespace WidgetProps {
-    /**
-     * Use for string / number / boolean widgets
-     */
-    interface BaseProps<ViewBag= { [key: string]: any }> {
-        value?: {};
-        onChange: (value?: {}) => void;
-        schema: Schema;
-        view: View<ViewBag>;
-        errorMessage?: string[];
-        editKey: string;
-        path: string[];
-    }
-    /**
-     * Use for Object widget
-     */
-    interface ObjectProps<ViewBag= { [key: string]: any }> extends BaseProps<ViewBag> {
-        value?: object;
-        schema: Schema.Object;
-        children?: (
-            | React.ComponentClass<WidgetProps>
-            | React.SFC<WidgetProps>)[];
-        addKey: (key: string, value?: {}) => void;
-        removeKey: (key: string) => void;
-        alterKey: (key: string, newKey: string) => void;
-    }
-    /**
-     * Use for Array widget
-     */
-    interface ArrayProps<ViewBag= { [key: string]: any }> extends BaseProps<ViewBag> {
-        value?: {}[];
-        schema: Schema.Array;
-        children?: (
-            | React.ComponentClass<WidgetProps>
-            | React.SFC<WidgetProps>)[];
-        onChildAdd: (value?: {}) => void;
-        onChildRemove: (index: number) => void;
-    }
+  /**
+   * Use for string / number / boolean widgets
+   */
+  interface BaseProps<ViewBag = {}> {
+    value?: {};
+    onChange: (value?: {}) => void;
+    schema: Schema<ViewBag>;
+    view: ViewBag;
+    errorMessage?: string[];
+    editKey: string;
+    path: string[];
+  }
+  /**
+   * Use for Object widget
+   */
+  interface ObjectProps<ViewBag = {}> extends BaseProps<ViewBag> {
+    value?: object;
+    schema: Schema.Object<ViewBag>;
+    children?: (React.ComponentClass<WidgetProps> | React.SFC<WidgetProps>)[];
+    addKey: (key: string, value?: {}) => void;
+    removeKey: (key: string) => void;
+    alterKey: (key: string, newKey: string) => void;
+  }
+  /**
+   * Use for Array widget
+   */
+  interface ArrayProps<ViewBag = {}> extends BaseProps<ViewBag> {
+    value?: {}[];
+    schema: Schema.Array<ViewBag>;
+    children?: (React.ComponentClass<WidgetProps> | React.SFC<WidgetProps>)[];
+    onChildAdd: (value?: {}) => void;
+    onChildRemove: (index: number) => void;
+  }
 }
 /**
  * Props passed in widgets.
  */
 type WidgetProps =
-    | WidgetProps.ArrayProps
-    | WidgetProps.BaseProps
-    | WidgetProps.ObjectProps;
+  | WidgetProps.ArrayProps
+  | WidgetProps.BaseProps
+  | WidgetProps.ObjectProps;
 
 type Action = (tree: any, path?: string[], ...args: any[]) => {} | void;
