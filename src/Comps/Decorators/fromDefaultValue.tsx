a31a253ae083daca6fbf0c789827c94a2cd37952
Cyril Junod <cyril.junod@gmail.com>
2018-05-24 14:54:25 +0200

    Update for React 16.4
    make more check before update

--- src/Comps/Decorators/fromDefaultValue.tsx
+++ src/Comps/Decorators/fromDefaultValue.tsx
@@ -4,49 +4,49 @@ import { setDefaultValue } from '../../Store/actions';
 import { Action, Schema } from '../../../typings/types';
 
 export type Props = {
-    editKey?: string;
-    path: string[];
-    status: {
-        $$$state?: string;
-    };
-    value?: {};
-    schema: Schema;
-    dispatch: (action: Action, ...args: ({} | undefined)[]) => any;
+  editKey?: string;
+  path: string[];
+  status: {
+    $$$state?: string;
+  };
+  value?: {};
+  schema: Schema;
+  dispatch: (action: Action, ...args: ({} | undefined)[]) => any;
 };
 function updateDefault({
-    value,
-    schema: { value: defaultValue },
-    dispatch,
-    path,
+  value,
+  schema: { value: defaultValue },
+  dispatch,
+  path,
 }: Props) {
-    const val = value !== undefined ? value : cloneDeep(defaultValue);
-    if (val !== value) {
-        dispatch(setDefaultValue, path, val);
-    }
-    return val;
+  const val = value !== undefined ? value : cloneDeep(defaultValue);
+  if (val !== value) {
+    dispatch(setDefaultValue, path, val);
+  }
+  return val;
 }
 
 function fromDefaultValue<P extends Props>(
-    Comp: React.ComponentClass<P> | React.SFC<P>
+  Comp: React.ComponentClass<P> | React.SFC<P>,
 ) {
-    class DefaultValue extends React.Component<P, { val?: {} }> {
-        static getDerivedStateFromProps(nextProps: P) {
-            if (nextProps.status.$$$state === undefined) {
-                /* 
-                Should avoid side effects, 
-                but in cDU, child is called before parent.
-                */
-                return { val: updateDefault(nextProps) };
-            }
-            return { val: nextProps.value };
-        }
-        state = { val: undefined };
-        render() {
-            return <Comp {...this.props} value={this.state.val} />;
-        }
+  class DefaultValue extends React.Component<P, { val?: {} }> {
+    static getDerivedStateFromProps(nextProps: P) {
+      if (nextProps.status.$$$state === undefined) {
+        /* 
+        Should avoid side effects, 
+        but in cDU, child is called before parent.
+        */
+        return { val: updateDefault(nextProps) };
+      }
+      return { val: nextProps.value };
     }
+    state = { val: undefined };
+    render() {
+      return <Comp {...this.props} value={this.state.val} />;
+    }
+  }
 
-    return DefaultValue;
+  return DefaultValue;
 }
 
 export default fromDefaultValue;
--- src/Store/index.tsx
+++ src/Store/index.tsx
@@ -2,84 +2,94 @@ import * as React from 'react';
 import immer from 'immer';
 
 interface StoreProps {
-    value?: {};
-    schema: {};
-    onValueChange: (value: {}) => void;
-    children: (
-        props: {
-            dispatch: (
-                action: (state: any, ...extraArgs: any[]) => void,
-                ...args: any[]
-            ) => void;
-            schema: {};
-            value: {};
-            status: {};
-        }
-    ) => JSX.Element;
+  value?: {};
+  schema: {};
+  onValueChange: (value: {}) => void;
+  children: (
+    props: {
+      dispatch: (
+        action: (state: any, ...extraArgs: any[]) => void,
+        ...args: any[]
+      ) => void;
+      schema: {};
+      value: {};
+      status: {};
+    },
+  ) => JSX.Element;
 }
 const FormContext = React.createContext<{
-    value?: {};
-    schema: {};
-    status: {};
+  value?: {};
+  schema: {};
+  status: {};
 }>({
-    value: undefined,
-    schema: {},
-    status: {},
+  value: undefined,
+  schema: {},
+  status: {},
 });
 export const FormConsumer = FormContext.Consumer;
 export class Store extends React.Component<StoreProps> {
-    state = {
-        schema: {},
-        value: {},
-        status: {},
-    };
-    static getDerivedStateFromProps(
-        nextProps: StoreProps,
-        state: { schema: {}; value: {}; status: {} }
+  state = {
+    schema: {},
+    value: {},
+    status: {},
+    oldProps: {},
+  };
+  static getDerivedStateFromProps(
+    nextProps: StoreProps,
+    state: {
+      schema: {};
+      value: {};
+      status: {};
+      oldProps: StoreProps;
+    },
+  ) {
+    if (state.oldProps !== nextProps) {
+      let ret: Partial<typeof Store.prototype.state> = {
+        value: nextProps.value,
+        schema: nextProps.schema,
+        oldProps: nextProps,
+      };
+      if (nextProps.value !== state.value) {
+        ret.status = {};
+      }
+      return ret;
+    }
+    return null;
+  }
+  dispatch = (
+    action: (state: any, ...extraArgs: any[]) => void,
+    ...args: any[]
+  ) => {
+    this.setState(prevState => {
+      return (immer(action) as (state: any, ...extraArgs: any[]) => any)(
+        prevState,
+        ...args,
+      );
+    });
+  };
+  shouldComponentUpdate(nextProps: any, nextState: any) {
+    return this.state !== nextState;
+  }
+  componentDidUpdate(prevProps: StoreProps, prevState: { value: {} }) {
+    if (
+      this.state.value !== prevState.value &&
+      // This is not an update due to a props change.
+      this.props.value === prevProps.value
     ) {
-        if (
-            state.schema !== nextProps.schema ||
-            state.value !== nextProps.value
-        ) {
-            return {
-                value: nextProps.value,
-                schema: nextProps.schema,
-                status: {},
-            };
-        }
-        return null;
+      this.props.onValueChange(this.state.value);
     }
-    dispatch = (
-        action: (state: any, ...extraArgs: any[]) => void,
-        ...args: any[]
-    ) => {
-        this.setState(prevState => {
-            return (immer(action) as (state: any, ...extraArgs: any[]) => any)(
-                prevState,
-                ...args
-            );
-        });
-    };
-    shouldComponentUpdate(nextProps: any, nextState: any) {
-        return this.state !== nextState;
-    }
-    componentDidUpdate(prevProps: StoreProps, prevState: { value: {} }) {
-        if (
-            this.state.value !== prevState.value &&
-            // This is not an update due to a props change.
-            this.props.value === prevProps.value
-        ) {
-            this.props.onValueChange(this.state.value);
-        }
-    }
-    render() {
-        return (
-            <FormContext.Provider value={this.state}>
-                {this.props.children({
-                    ...this.state,
-                    dispatch: this.dispatch,
-                })}
-            </FormContext.Provider>
-        );
-    }
+  }
+  render() {
+    const { schema, value, status } = this.state;
+    return (
+      <FormContext.Provider value={this.state}>
+        {this.props.children({
+          schema,
+          value,
+          status,
+          dispatch: this.dispatch,
+        })}
+      </FormContext.Provider>
+    );
+  }
 }
