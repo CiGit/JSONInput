41bc004ce8c568cc991dfdf98496d65b58121c9b
Cyril Junod <cyril.junod@gmail.com>
2018-05-24 15:17:41 +0200

    Update 0.0.11

--- rollup-view.config.js
+++ rollup-view.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
@@ -12,39 +12,39 @@ const pkg = require('./package.json');
 const env = process.env.NODE_ENV;
 
 const external = Object.keys(pkg.dependencies).concat(
-    Object.keys(pkg.peerDependencies)
+  Object.keys(pkg.peerDependencies),
 );
 export default {
-    input: 'src/Comps/Views/index.ts',
-    plugins: [
-        nodeResolve({
-            jsnext: true,
-            main: true,
-        }),
-        replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
-        typescript({
-            typescript: require('typescript'),
-        }),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-        commonjs(),
-        env === 'production' && uglify({ mangle: { toplevel: true } }, minify),
-        filesize(),
-    ],
-    external: function ext(module) {
-        return external.indexOf(module.split('/')[0]) > -1;
+  input: 'src/Comps/Views/index.ts',
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+    }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+    typescript({
+      typescript: require('typescript'),
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    env === 'production' && uglify({ mangle: { toplevel: true } }, minify),
+    filesize(),
+  ],
+  external: function ext(module) {
+    return external.indexOf(module.split('/')[0]) > -1;
+  },
+  output: [
+    {
+      file: 'lib/views.js',
+      format: 'cjs',
+      sourcemap: true,
     },
-    output: [
-        {
-            file: 'lib/views.js',
-            format: 'cjs',
-            sourcemap: true,
-        },
-        {
-            file: 'lib/views.es2015.js',
-            format: 'es',
-            sourcemap: true,
-        },
-    ],
+    {
+      file: 'lib/views.es2015.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
@@ -12,36 +12,36 @@ import * as pkg from './package.json';
 const env = process.env.NODE_ENV;
 
 const external = Object.keys(pkg.peerDependencies).concat(
-    Object.keys(pkg.dependencies)
+  Object.keys(pkg.dependencies),
 );
 export default {
-    input: 'src/index.ts',
-    plugins: [
-        nodeResolve({}),
-        replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
-        typescript({
-            typescript: require('typescript'),
-        }),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-        commonjs(),
-        env === 'production' && uglify({ mangle: { toplevel: true } }, minify),
-        filesize(),
-    ],
-    external: function ext(module) {
-        return external.includes(module);
+  input: 'src/index.ts',
+  plugins: [
+    nodeResolve({}),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+    typescript({
+      typescript: require('typescript'),
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    env === 'production' && uglify({ mangle: { toplevel: true } }, minify),
+    filesize(),
+  ],
+  external: function ext(module) {
+    return external.includes(module);
+  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
     },
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true,
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            sourcemap: true,
-        },
-    ],
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
 };
