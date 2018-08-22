fa685938e0b6cd2214f8429cba5d97deb144a446
Cyril Junod <cyril.junod@gmail.com>
2018-08-22 13:05:55 +0200

    Make labels nearly correct

--- src/Comps/Views/labeled.js
+++ src/Comps/Views/labeled.js
@@ -2,61 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function labeled(Comp) {
-    function Label(props) {
-        const required = props.schema.required;
-        const requiredClassName = required ? 'required' : '';
-        return (
-            <div>
-                <label //eslint-disable-line
-                    className={`${props.schema.type}Field ${requiredClassName}`}
-                >
-                    <span className="title">
-                        {props.view.title || props.editKey}
-                    </span>
-                    <Comp {...props} />
-                    <span>{props.view.description}</span>
-                    <span>{props.errorMessage}</span>
-                </label>
-            </div>
-        );
-    }
+  function Label(props) {
+    const required = props.schema.required;
+    const requiredClassName = required ? 'required' : '';
+    return (
+      <div>
+        <label className={`${props.schema.type}Field ${requiredClassName}`}>
+          <span className="title">{props.view.title || props.editKey}</span>
+        </label>
+        <Comp {...props} />
+        <span>{props.view.description}</span>
+        <span>{props.errorMessage}</span>
+      </div>
+    );
+  }
 
-    Label.propTypes = {
-        schema: PropTypes.shape({
-            type: PropTypes.oneOfType([
-                PropTypes.oneOf([
-                    'object',
-                    'string',
-                    'number',
-                    'array',
-                    'boolean'
-                ]),
-                PropTypes.arrayOf(
-                    PropTypes.oneOf([
-                        'object',
-                        'string',
-                        'number',
-                        'array',
-                        'boolean',
-                        'null'
-                    ])
-                )
-            ]).isRequired,
-            required: PropTypes.bool,
-            description: PropTypes.string
-        }).isRequired,
-        editKey: PropTypes.string,
-        view: PropTypes.shape({
-            title: PropTypes.string,
-            description: PropTypes.string
-        }).isRequired,
-        errorMessage: PropTypes.arrayOf(PropTypes.string)
-    };
-    Label.defaultProps = {
-        editKey: '',
-        errorMessage: []
-    };
-    return Label;
+  Label.propTypes = {
+    schema: PropTypes.shape({
+      type: PropTypes.oneOfType([
+        PropTypes.oneOf(['object', 'string', 'number', 'array', 'boolean']),
+        PropTypes.arrayOf(
+          PropTypes.oneOf([
+            'object',
+            'string',
+            'number',
+            'array',
+            'boolean',
+            'null',
+          ]),
+        ),
+      ]).isRequired,
+      required: PropTypes.bool,
+      description: PropTypes.string,
+    }).isRequired,
+    editKey: PropTypes.string,
+    view: PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+    }).isRequired,
+    errorMessage: PropTypes.arrayOf(PropTypes.string),
+  };
+  Label.defaultProps = {
+    editKey: '',
+    errorMessage: [],
+  };
+  return Label;
 }
 
 export default labeled;
