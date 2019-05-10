432f8925c9136eff214b9de15c5dc92b2f4806ce
Cyril Junod <cyril.junod@gmail.com>
2019-05-10 14:57:41 +0200

    dependencies update

--- lib/views.es2015.d.ts
+++ lib/views.es2015.d.ts
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
 type View = Record<
-    | 'string'
-    | 'number'
-    | 'object'
-    | 'boolean'
-    | 'array'
-    | 'arrowNumber'
-    | 'select'
-    | 'hashmap',
-    React.ComponentType
+  | 'string'
+  | 'number'
+  | 'object'
+  | 'boolean'
+  | 'array'
+  | 'arrowNumber'
+  | 'select'
+  | 'hashmap',
+  React.ComponentType
 >;
 declare const view: View;
 export default view;
