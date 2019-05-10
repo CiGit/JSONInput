0c36ebd7fa35a0b8ef2b26d4b08f4fa21d12f5c7
maxencelaurent <maxence.laurent@gmail.com>
2019-05-10 14:35:13 +0200

    Add path parameter to "errored" function (#2)

--- src/Utils/customValidator.ts
+++ src/Utils/customValidator.ts
@@ -6,12 +6,19 @@ const customValidator = new jsonschema.Validator();
 customValidator.attributes.errored = function validateErrored(
   instance: {},
   schema: { errored?: ErrorFn },
-  options: { formValue: {} },
+  options: { formValue: {}, ctx: {basePath?: string[]} },
+  ctx : { propertyPath : String},
 ) {
   if (typeof schema.errored !== 'function') {
     throw new jsonschema.SchemaError('"errored" expects a function');
   }
-  const msg = schema.errored(instance, options.formValue);
+    const path = (options.ctx.basePath || [])
+      .concat(ctx.propertyPath.split('.').slice(1));
+    const msg = schema.errored(
+      instance,
+      options.formValue,
+      path,
+    );
   if (msg) {
     return msg;
   }
@@ -21,9 +28,13 @@ function validate(
   value: {} | undefined,
   schema: Schema,
   formValue?: {},
+  basePath?: string[],
 ): ValidatorResult {
   return customValidator.validate(value, schema, {
     formValue: formValue || {},
+    ctx: {
+        basePath,
+    }
   });
 }
 
