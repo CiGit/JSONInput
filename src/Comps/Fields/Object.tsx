5fc6774b350afb058669fa7733789bcdf41de8f1
Cyril Junod <cyril.junod@gmail.com>
2019-08-15 15:46:26 +0200

    Object Fix React keys
    Object may reorder their properties. As such when changing a key, They may move arround.
    They still move arround but focus is kept. (hashMap mode)

--- src/Comps/Fields/Object.tsx
+++ src/Comps/Fields/Object.tsx
@@ -56,96 +56,109 @@ function schemaForProp(schema: Schema.Object, prop: string) {
 }
 const EMPTY_OBJECT = {};
 
-function renderChildren(props: Props) {
-  const children = [];
-  const properties = props.schema.properties || {};
-  const value: { [key: string]: {} } = props.value || {};
-  // Holds schema properties and value properties missing from schema.
-  const mergedProperties: Array<string> = Object.keys(properties);
+class ObjectField extends React.Component<Props> {
+  keys = new Map<string, string>();
+  keyId = 0;
+  renderChildren(props: Props) {
+    const children = [];
+    const properties = props.schema.properties || {};
+    const value: { [key: string]: {} } = props.value || {};
+    // Holds schema properties and value properties missing from schema.
+    const mergedProperties: Array<string> = Object.keys(properties);
 
-  Object.keys(value).forEach(v => {
-    if (v in properties) {
-      return;
-    }
-    mergedProperties.push(v);
-  });
-  function indexFor(property: string): number {
-    if (properties[property]) {
-      const index = properties[property].index;
-      if (typeof index === 'number') {
-        return index;
+    Object.keys(value).forEach(v => {
+      if (v in properties) {
+        return;
       }
+      mergedProperties.push(v);
+    });
+    function indexFor(property: string): number {
+      if (properties[property]) {
+        const index = properties[property].index;
+        if (typeof index === 'number') {
+          return index;
+        }
+      }
+      return 0;
     }
-    return 0;
-  }
-  // Index based sorting
-  function sortProperties(a: string, b: string): number {
-    return indexFor(a) - indexFor(b);
+    // Index based sorting
+    function sortProperties(a: string, b: string): number {
+      return indexFor(a) - indexFor(b);
+    }
+
+    mergedProperties.sort(sortProperties);
+    const keys = new Map<string, string>();
+    for (let i: number = 0; i < mergedProperties.length; i += 1) {
+      const prop: string = mergedProperties[i];
+      const propSchema = schemaForProp(props.schema, prop);
+      if (!this.keys.has(prop)) {
+        keys.set(prop, String(this.keyId++));
+      } else {
+        keys.set(prop, this.keys.get(prop)!);
+      }
+      children.push(
+        <SchemaType
+          {...props}
+          status={props.status[prop] || EMPTY_OBJECT}
+          schema={propSchema}
+          value={value[prop]}
+          editKey={prop}
+          key={keys.get(prop)}
+        />,
+      );
+    }
+    this.keys = keys;
+    return children;
   }
 
-  mergedProperties.sort(sortProperties);
-  for (let i: number = 0; i < mergedProperties.length; i += 1) {
-    const prop: string = mergedProperties[i];
-    const propSchema = schemaForProp(props.schema, prop);
-    children.push(
-      <SchemaType
-        {...props}
-        status={props.status[prop] || EMPTY_OBJECT}
-        schema={propSchema}
-        value={value[prop]}
-        editKey={prop}
-        key={i}
-      />,
-    );
-  }
-  return children;
-}
-
-function ObjectField(props: Props) {
-  function addKey(key: string, value: {}): void {
-    if (typeof props.value === 'object' && key in props.value) {
+  addKey = (key: string, value?: unknown): void => {
+    if (typeof this.props.value === 'object' && key in this.props.value) {
       throw new Error(`Property "${key}" already exists`);
     }
-    props.onChange(
-      Object.assign({}, props.value, {
+    this.keys.set(key, String(this.keyId++));
+    this.props.onChange(
+      Object.assign({}, this.props.value, {
         [key]: value,
       }),
     );
-  }
-
-  function removeKey(key: string): void {
-    const value: { [key: string]: {} } = Object.assign({}, props.value);
+  };
+  removeKey = (key: string): void => {
+    const value: { [key: string]: {} } = Object.assign({}, this.props.value);
     delete value[key];
-    props.onChange(value);
-  }
-
-  function alterKey(key: string, newKey: string): void {
+    this.keys.delete(key);
+    this.props.onChange(value);
+  };
+  alterKey = (key: string, newKey: string): void => {
     if (key === newKey) {
       return;
     }
-    if (newKey in props.value) {
+    if (newKey in this.props.value) {
       throw new Error(`Property "${newKey}" already exists`);
     }
     const value: { [key: string]: {} } = {};
-    Object.keys(props.value).forEach(p => {
+    Object.keys(this.props.value).forEach(p => {
       if (p !== key) {
-        value[p] = props.value[p];
+        value[p] = this.props.value[p];
       } else {
-        value[newKey] = props.value[p];
+        value[newKey] = this.props.value[p];
       }
     });
-    props.onChange(value);
+    const currentElementKey = this.keys.get(key);
+    this.keys.delete(key);
+    this.keys.set(newKey, currentElementKey!);
+    this.props.onChange(value);
+  };
+  render() {
+    return (
+      <Widget
+        {...this.props}
+        addKey={this.addKey}
+        removeKey={this.removeKey}
+        alterKey={this.alterKey}
+      >
+        {this.renderChildren(this.props)}
+      </Widget>
+    );
   }
-  return (
-    <Widget
-      {...props}
-      addKey={addKey}
-      removeKey={removeKey}
-      alterKey={alterKey}
-    >
-      {renderChildren(props)}
-    </Widget>
-  );
 }
-
 export default validator(ObjectField);
